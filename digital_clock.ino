73eef135fa950a61fca5ea9e4f335e0b47255cce
applejackman <jackson@norphar.com>
2015-10-01 10:47:42 -0500

    made all lights blink at 1`/2 a sec

--- digital_clock.ino
+++ digital_clock.ino
@@ -1,17 +1,86 @@
-// Arduino Clock
+// Jackson
 //
 
 #include "SevSeg.h"
 
 #define d1 13
 #define a 2
+#define f 7
+#define d2 12
+#define d3 11
+#define b 3
+#define e 6
+#define d 5
+#define dp 9
+#define c 4
+#define g 8
+#define d4 10
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(d1, OUTPUT);
+  pinMode(a, OUTPUT);
+  pinMode(f, OUTPUT);
+  pinMode(d2, OUTPUT);
+  pinMode(d3, OUTPUT);
+  pinMode(b, OUTPUT);
+  pinMode(e, OUTPUT);
+  pinMode(d, OUTPUT);
+  pinMode(dp, OUTPUT);
+  pinMode(c, OUTPUT);
+  pinMode(g, OUTPUT);
+  pinMode(d4, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  digitalWrite(d1, HIGH);
+  delay(50);
+  digitalWrite(d1, LOW);
+  delay(50);
+  digitalWrite(a, HIGH);
+  delay(50);
+  digitalWrite(a, LOW);
+  delay(50);
+  digitalWrite(f, HIGH);
+  delay(50);
+  digitalWrite(f, LOW);
+  delay(50);
+  digitalWrite(d2, HIGH);
+  delay(50);
+  digitalWrite(d2, LOW);
+  delay(50);
+  digitalWrite(d3, HIGH);
+  delay(500);
+  digitalWrite(d3, LOW);
+  delay(500);
+  digitalWrite(b, HIGH);
+  delay(500);
+  digitalWrite(b, LOW);
+  delay(500);
+  digitalWrite(e, HIGH);
+  delay(500);
+  digitalWrite(e, LOW);
+  delay(500);
+  digitalWrite(d, HIGH);
+  delay(500);
+  digitalWrite(d, LOW);
+  delay(500);
+  digitalWrite(dp, HIGH);
+  delay(500);
+  digitalWrite(dp, LOW);
+  delay(500);
+  digitalWrite(c, HIGH);
+  delay(500);
+  digitalWrite(c, LOW);
+  delay(500);
+  digitalWrite(g, HIGH);
+  delay(500);
+  digitalWrite(g, LOW);
+  delay(500);
+  digitalWrite(d4, HIGH);
+  delay(500);
+  digitalWrite(d4, LOW);
+  delay(500);
 }
